e41158d0ff92fd744703ad0ea9732153834c05c8
Sergii Ivanchenko <sergiiivanchenko@Sergiis-MacBook-Pro.local>
2016-07-28 10:23:54 +0100

    NodeJS. Week2. Node and MongoDB part1 and 2 completed. Lesson 3 from 5

--- node-mongodb/simpleserver.js
+++ node-mongodb/simpleserver.js
@@ -1,21 +1,72 @@
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
-//Connection URL
+//connection URL
 var url = 'mongodb://localhost:27017/conFusion';
-// Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
-   assert.equal(err, null);
-   console.log("connected correctly to server");
+   assert.equal (err, null);
+   console.log("Connected correctly to server");
     
-    var collection = db.collection("dishes");
+   var collection = db.collection("dishes") ;
     
-    collection.insertOne({name:"Uthapizza2", description: "test2"}, 
-        function (err, result) {
-            assert.equal(err,null);
-            console.log("After insert:");
-            console.log(result.ops);
-        
+// show current docs in collection
+    collection.find({}).toArray(function (err, docs) {
+           assert.equal(err, null);
+            console.log("Found:");
+            console.log(docs);
         });
     
+    collection.insertOne ({name:"Uthapizza", description: "test"}, function (err, result) {
+        assert.equal(err, null);
+        console.log("After Insert:");
+        console.log(result.ops);
+        
+        collection.find({}).toArray(function (err, docs) {
+           assert.equal(err, null);
+            console.log("Found:");
+            console.log(docs);
+//            db.close();
+        
+        db.dropCollection("dishes", function (err, result) {
+           assert.equal(err, null);
+            db.close();
+            
+        });
+            
+        });
+        
+        
+    });
+    
+    
+    
 });
+
+
+
+
+
+
+
+
+//var MongoClient = require('mongodb').MongoClient,
+//    assert = require('assert');
+//
+////Connection URL
+//var url = 'mongodb://localhost:27017/conFusion';
+//// Use connect method to connect to the Server
+//MongoClient.connect(url, function (err, db) {
+//   assert.equal(err, null);
+//   console.log("connected correctly to server");
+//    
+//    var collection = db.collection("dishes");
+//    
+//    collection.insertOne({name:"Uthapizza2", description: "test2"}, 
+//        function (err, result) {
+//            assert.equal(err,null);
+//            console.log("After insert:");
+//            console.log(result.ops);
+//        
+//        });
+//    
+//});
